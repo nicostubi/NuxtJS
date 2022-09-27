853b758a1469bad3da59569f68b90b2c92e50ccb
Nicolas Stübi <nicolas.stubi@spma.ch>
2022-09-27 14:46:11 +0200

    Pages and Routing - lesson 1

---
+++ components/HomeCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+  <img :src="home.images[0]" style="width:200px" /><br />
+  {{ home.title}}<br />
+  {{ home.location.address}}{{ home.location.city}}{{ home.location.state}}<br />
+  {{ home.guests}} guests<br />
+  {{ home.pricePerNight}} / night<br />
+  </div>
+</template>
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default {
+  props: {
+    home: 
+    {
+    type: Object,
+      required: true
+    }
+  }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,23 @@
-<template><div>homepage</div></template>
+<template>
+<div>
+  <div v-for="home in homes" :key="home.objectID" style="float:left;margin:10px">
+    <home-card :home="home"></home-card>
+  </div>
+</div>
+</template>
+<script>
+import homes from '~/data/homes'
+import HomeCard from '~/components/HomeCard'
+
+export default {
+  components: {
+    HomeCard,
+  },
+  data() {
+    return {
+      homes: homes.slice(0, 3)
+        
+      }
+    }
+  }
+</script>
