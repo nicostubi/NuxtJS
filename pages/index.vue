6656024bb4d2eb4705b0d0d03049152792b9512e
Nicolas Stübi <nicolas.stubi@spma.ch>
2022-09-27 15:03:15 +0200

    Pages and Routing - lesson 2

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,21 @@
 <template>
-<div>
-  <div v-for="home in homes" :key="home.objectID" style="float:left;margin:10px">
-    <home-card :home="home"></home-card>
+  <div>
+    <div v-for="home in homes" :key="home.objectID" style="float:left;margin:10px">
+      <home-card :home="home" />
+    </div>
   </div>
-</div>
 </template>
 <script>
 import homes from '~/data/homes'
 import HomeCard from '~/components/HomeCard'
-
 export default {
   components: {
-    HomeCard,
+    HomeCard
   },
   data() {
     return {
       homes: homes.slice(0, 3)
-        
-      }
     }
   }
+}
 </script>
